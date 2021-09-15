a11760c90f29602fa662808e986ef8b6f628c5e4
RikuRuokonen <riku.ruokonen@helsinki.fi>
2021-09-16 00:48:03 +0300

    added base of maze generator without wall-building functions and basic maze logging

--- src/com/ruokonen/Main.java
+++ src/com/ruokonen/Main.java
@@ -1,8 +1,12 @@
 package com.ruokonen;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("testing");
+        MazeBuilder mb = new MazeBuilder(10, 10);
+        System.out.println(Arrays.deepToString(mb.getMaze()).replace("], ", "]\n").replace("[[", "[").replace("]]", "]"));
     }
 }
---
+++ src/com/ruokonen/MazeBuilder.java
@@ -0,0 +1,19 @@
+package com.ruokonen;
+
+public class MazeBuilder {
+
+    private char[][] maze;
+
+    public MazeBuilder(int x, int y) {
+        maze = new char[y][x];
+        for(int i = 0; i < maze.length; i++) {
+            for(int j = 0; j < maze[0].length; j++) {
+                maze[i][j] = '.';
+            }
+        }
+    }
+
+    public char[][] getMaze() {
+        return maze;
+    }
+}
